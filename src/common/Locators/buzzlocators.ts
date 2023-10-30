36754a791a85e32378bee8f8d95f9cd1369eecb3
yaswanthleburu <yaswanth.leburu@atmecs.com>
2023-10-30 16:28:57 +0530

    Updated

--- src/common/Locators/buzzlocators.ts
+++ src/common/Locators/buzzlocators.ts
@@ -14,6 +14,27 @@ export class Buzzlocators{
     readonly videourl : string;
     readonly sharevideobutton : string;
     readonly verificationcomment : string;
+    readonly mostRecentPosts : string;
+    readonly mostLikedPosts : string;
+    readonly mostCommentedPosts : string;
+    readonly mostRecentpostdata : string;
+    readonly mostLikedPostsdata : string;
+    readonly mostCommentedPostsdata : string;
+    readonly threeDots : string;
+    readonly deletePost : string;
+    readonly yesDeletePost : string;
+    readonly postComment : string;
+    readonly writeComment : string;
+    readonly postShareIcon : string;
+    readonly postSharedata : string;
+    readonly shareInShareIcon : string;
+    readonly postHeart : string;
+    readonly postLikecount : string;
+    readonly sharecount : string;
+    readonly commentCount : string;
+    readonly upcomingAnniversaries : string;
+    readonly upcomingAnniversariesdata : string;
+    
     
     constructor(page: Page){
         this.page = page
@@ -27,7 +48,30 @@ export class Buzzlocators{
         this.sharevideo ="//button[text()=' Share Video']";
         this.videourl="//textarea[@placeholder='Paste Video URL']";
         this.sharevideobutton ="//button[text()=' Share ']";
-        this.verificationcomment="(//div[@class='orangehrm-buzz-post-header'])[1]"
+        this.verificationcomment="(//div[@class='orangehrm-buzz-post-header'])[1]";
+        this.mostRecentPosts="//button[text()=' Most Recent Posts ']";
+        this.mostLikedPosts = "//button[text()=' Most Liked Posts ']";
+        this.mostCommentedPosts =  "//button[text()=' Most Commented Posts ']";
+        this.mostRecentpostdata ="//div[@class='oxd-grid-1 orangehrm-buzz-newsfeed-posts']";
+        this.mostLikedPostsdata="//div[@class='oxd-grid-1 orangehrm-buzz-newsfeed-posts']";
+        this.mostCommentedPostsdata="//div[@class='oxd-grid-1 orangehrm-buzz-newsfeed-posts']";
+        this.threeDots="(//button[@class='oxd-icon-button'])[2]";
+        this.deletePost="//p[text()='Delete Post']";
+        this.yesDeletePost ="//button[text()=' Yes, Delete ']";
+        this.postHeart="(//div[@class='orangehrm-buzz-post-actions']//div)[1]";
+        this.postLikecount= '(//p[@class="oxd-text oxd-text--p"])[1]';
+
+        this.postComment="(//div[@class='orangehrm-like-animation']/following-sibling::button)[1]";
+        this.writeComment="//input[@placeholder='Write your comment...']";
+        this.commentCount='(//p[@class="oxd-text oxd-text--p orangehrm-buzz-stats-active"])[1]';
+        
+        this.postShareIcon = "(//button[@class='oxd-icon-button']/following-sibling::button)[1]";
+        this.postSharedata="(//div[@class='oxd-buzz-post oxd-buzz-post--active']//textarea)[2]";
+        this.shareInShareIcon ="//button[text()=' Share ']";
+        this.sharecount= "(//p[@class='oxd-text oxd-text--p orangehrm-buzz-stats-active'])[2]";
+        this.upcomingAnniversaries ="//p[text()='Upcoming Anniversaries']/following-sibling::div";
+        this.upcomingAnniversariesdata="div.orangehrm-buzz-anniversary-nocontent"
+        
     }
 }
 
--- src/pages/buzzPage.ts
+++ src/pages/buzzPage.ts
@@ -33,8 +33,42 @@ export  class Buzzmenu {
         await this.page.locator(this.buzzlocators.sharevideo).click();
         await this.page.locator(this.buzzlocators.videourl).fill("https://www.youtube.com/watch?v=OI0F-zEhPJw");
         await this.page.locator(this.buzzlocators.sharevideobutton).click();
+        
         await (await this.page.waitForSelector(this.buzzlocators.verificationcomment)).isVisible();
         const data = await this.page.locator(this.buzzlocators.verificationcomment).textContent();
-        console.log("result",data)
+        console.log("result",data);
+
+        await this.page.locator(this.buzzlocators.postHeart).click();
+        const like = await this.page.locator(this.buzzlocators.postLikecount).textContent();
+        console.log("like result",like);
+
+        await this.page.locator(this.buzzlocators.postComment).click();
+        await this.page.locator(this.buzzlocators.writeComment).fill("nice");
+        await this.page.keyboard.press('Enter');
+        const comment = await this.page.locator(this.buzzlocators.commentCount).textContent();
+        console.log("comment result",comment);
+
+        await this.page.locator(this.buzzlocators.postShareIcon).click();
+        await this.page.locator(this.buzzlocators.postSharedata).fill("good");
+        await this.page.locator(this.buzzlocators.shareInShareIcon).click();
+        const share = await this.page.locator(this.buzzlocators.sharecount).textContent();
+        console.log("share result",share);
+        
+        await this.page.locator(this.buzzlocators.mostRecentPosts).click();
+        let recentpost = await (await this.page.waitForSelector(this.buzzlocators.mostRecentpostdata)).isVisible();
+        console.log("mostrecentpostValidation",recentpost)
+        await this.page.locator(this.buzzlocators.mostLikedPosts).click();
+        let Likedpost = await (await this.page.waitForSelector(this.buzzlocators.mostLikedPostsdata)).isVisible();
+        console.log("mostlikedpostValidation",Likedpost)
+        await this.page.locator(this.buzzlocators.mostCommentedPosts).click();
+        let Commentedpost = await (await this.page.waitForSelector(this.buzzlocators.mostCommentedPostsdata)).isVisible();
+        console.log("mostCommentedpostValidation",Commentedpost)
+        await this.page.locator(this.buzzlocators.threeDots).click();
+        await this.page.locator(this.buzzlocators.deletePost).click();
+        await this.page.locator(this.buzzlocators.yesDeletePost).click();
+        let UpcomingAnniversaries = await (await this.page.waitForSelector(this.buzzlocators.upcomingAnniversaries)).isVisible();
+        console.log("Upcoming Anniversaries",UpcomingAnniversaries);
+        const Anniversaries = await this.page.locator(this.buzzlocators.upcomingAnniversariesdata).textContent();
+        console.log("Upcoming Anniversaries data",Anniversaries);
     }
 }
